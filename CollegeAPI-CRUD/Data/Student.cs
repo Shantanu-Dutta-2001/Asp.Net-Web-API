dfb60cc0df0887e3a48dc8bd66c4dc0be24d623f
ShantanuDutta-PT <shantanudutta07@gmail.com>
2024-12-29 22:13:26 +0530

    Setup dbcontext file for db connection EF Code first approach

---
+++ CollegeAPI-CRUD/Data/Student.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeAPI_CRUD.Data
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+
+    }
+}
